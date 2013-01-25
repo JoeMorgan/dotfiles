36f7d729ce0d9e9fa5435852a192645749a7c6bf
JoeMorgan <josepht.morgan@gmail.com>
2013-01-24 20:38:24 -0500

    Fixing issue with zsh

--- sync.sh
+++ sync.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
 cd "$(dirname "$0")"
 git pull
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . ~
 }
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" = "--force" -o "$1" = "-f" ]; then
 	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo -n "This may overwrite existing files in your home directory. Are you sure? (y/n) "
+	read -n 1 response 
+	if [[ $response =~ ^[Yy]$ ]]; then
 		doIt
 	fi
 fi
